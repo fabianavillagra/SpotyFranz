b2dbff47aab4ecf3f748c71a3ff010d73bdb03ae
fabianavillagra <128808535+fabianavillagra@users.noreply.github.com>
2023-04-28 09:24:12 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+class Cancion:
+    def __init__(self,titulo,categoria,idioma):
+        self.titulo = titulo
+        self.categoria = categoria
+        self.idioma = idioma
+        
+cancion1 = Cancion('La guitarra','Pop','Catellano')
+cancion2 = Cancion('Para no verte más','Pop','Catellano')
+cancion3 = Cancion('Balada para un gordo','Balada','Catellano')
+lista = [cancion1,cancion2,cancion3]
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('listar.html')
+    return render_template('listar.html', titulo='Canciones', musicas=lista)
 
-app.run(host="0.0.0.0", port=5000)
+@app.route('/nuevoregistro')
+def nuevoregistro():
+    return render_template('nuevoRegistro.html',titulo='Nueva Canción')
+
+@app.route('/crear',methods=['POST',])
+def crear():
+    titulo =request.form['titulo']
+    categoria =request.form['categoria']
+    idioma =request.form['idioma']
+    cancion =Cancion(tittulo,categoria,idioma)
+    lista.append(cancion)
+    return render_template('listar.html', titulo='Canciones', musicas=lista)
+
+app.run(host="0.0.0.0",port=5000)
